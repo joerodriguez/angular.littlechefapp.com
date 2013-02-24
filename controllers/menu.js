0598dfe000b33ce7c9fc1962301a9b0189781430
Joseph Rodriguez <joerodriguez@gmail.com>
2013-02-24 11:31:44 -0500

    remove compiled js

--- controllers/menu.js
+++
@@ -1,8 +0,0 @@
-function MenuController ( $scope ) {
-  $scope.user = { 
-    admin: false,
-    picture: 'http://a0.twimg.com/profile_images/75834218/n575683589_1343598_1804_normal.jpg',
-    login: 'joerodriguez'
-  };
-
-}
